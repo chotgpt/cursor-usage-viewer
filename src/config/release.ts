8074f0e5c4d6cf30db40c861bec0fc5d1f24ead3
gc <923169932@qq.com>
2026-08-30 23:13:44 +0800

    build: configure chotgpt release identity

--- src/config/release.ts
+++ src/config/release.ts
@@ -1,1 +1,1 @@
-export const RELEASE_PAGE_URL = "https://github.com/OWNER/cursor-usage-viewer/releases/latest";
+export const RELEASE_PAGE_URL = "https://github.com/chotgpt/cursor-usage-viewer/releases/latest";
